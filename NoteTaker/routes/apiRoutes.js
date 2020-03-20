0767267de4b6f97f77928cdf506f65465edfd2c1
Kiyole <Kasmith8100@gmail.com>
2020-03-19 20:23:30 -0400

    fixing api route file

--- NoteTaker/routes/apiRoutes.js
+++ NoteTaker/routes/apiRoutes.js
@@ -1,39 +1,57 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
+const path = require('path');
 let notes = require("../db/db.json");
 
+function getData(){
+let notes = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf-8')
+return JSON.parse(notes);
+}
+//getData();
+
 router.get("/api/notes", function(req, res){
-    res.json(notes);
+    res.json(getData());
 })
 
 router.post("/api/notes", function(req, res){
     //let newNote = req.body;
-
-    if(notes.length < 1){
+    
+    let note = req.body;
+    if(test = true){
         req.body.id = 1
+        console.log(note.length);
+        console.log(note);
+        
+        //write;
     } else {
-        let lastID = notes[notes.length -1].id
-        req.body.id = lastID + 1
+        let lastID = note.id + 1
+        req.body.id = parseInt(lastID)
+        console.log(req.body.id);
+        console.log(lastID);
+        //write;
     }
-    console.log(req.body);
-    notes.push(req.body);
+    //notes.push(req.body);
     fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    console.log(req.body);
+    //notes.push(req.body);
+    
     //path/name of file db.json
     //content notes (stringify)
-    res.json(true);
+    res.json(getData);
 })
 
 router.delete('/api/notes/:_id', function (req, res){
+    
     let ID = req.params._id;
     let newNotes = notes.filter(function (e){
        return e.id != ID ;
         
     });
     fs.writeFileSync('./db/db.json', JSON.stringify(newNotes))
-    res.json(newNotes); 
+    res.json(getData);
     console.log(ID)
-     console.log(newNotes)
+    console.log(newNotes)
     
 
  });
